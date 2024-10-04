da46fbfcd4d30be8ad25d49555d2461ab5599c1f
André Nordstrand <andre.nordstrand@lyse.no>
2024-10-04 21:14:11 +0200

    Keyword name

--- src/app/ui/share.tsx
+++ src/app/ui/share.tsx
@@ -60,7 +60,7 @@ export const Share = ({ keyword }: { keyword: Keyword }) => {
       {!keyword.hideImageShare && (
         <a
           className={globalStyles.button}
-          href={`/images/keywords/${keyword}.png`}
+          href={`/images/keywords/${keyword.name}.png`}
           download={true}
         >
           Download sharable image
